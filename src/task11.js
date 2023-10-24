5a3aa3e6fadd8e274e37ddc3ec3a8dbb20700bb1
Ostapchenko <ostapchenko2005@gmail.com>
2023-10-24 12:44:48 +0300

    task11

---
+++ src/task11.js
@@ -0,0 +1,16 @@
+const calcTime = (seconds) => {
+    minutes = 0;
+    hours = 0;
+    i = seconds;
+    while (i >=3600) {
+        hours += 1;
+        i -= 3600;
+    };
+    while (i >= 60){
+        minutes += 1;
+        i -= 60;
+    };
+    return console.log(`${hours} ч ${minutes} мин ${i}`);
+};
+
+calcTime(3665);
